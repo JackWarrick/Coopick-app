267d07b37d076ddb4e44b71a09aee271fb760d8a
hrcoop16 <hrcoop16@gmail.com>
2023-03-26 22:10:55 -0500

    added to back end

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controller");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 
-app.get('/', (req, res) => {
-    res.send("server running on port 3001")
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3001, () => {
-    console.log("server running on port 3001")
+app.use(routes);
 
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
