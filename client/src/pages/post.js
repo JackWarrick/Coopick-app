0db930e597dd4b215e89307a11661d364e126cb7
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2023-04-15 17:25:40 -0500

    More on signup, login, logout

--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logout from '../components/logout';
 //import profilepic from '../../assets/me.jpeg';
 //import "../../styles/about.css"
 import { Button } from 'react-bootstrap'
@@ -8,6 +9,7 @@ export default function Post() {
         return (
             
         <div className="App">
+        <Logout/>
         <h1>Blog Posts</h1>
         <Button>Test Button</Button>
         <div className="form">
