321f676455f41f7673eaafe6fa8a5f2569e807d9
hrcoop16 <hrcoop16@gmail.com>
2023-05-06 13:57:54 -0500

    Sign up successful

--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import Logout from '../components/logout';
+import React from "react";
+import Logout from "../components/logout";
 //import profilepic from '../../assets/me.jpeg';
 //import "../../styles/about.css"
-import { Button } from 'react-bootstrap'
-import Auth from '../utils/auth';
+import { Button } from "react-bootstrap";
+import Auth from "../utils/auth";
 
 export default function Post() {
-
-  if (!Auth.loggedIn()) {
+  if (Auth.loggedIn()) {
     console.log(Auth.loggedIn);
     return window.location.assign("/");
-  } else {   
-      
-        return (
-            
-        <div className="App">
-        <Logout/>
+  } else {
+    return (
+      <div className="App">
+        <Logout />
         <h1>Blog Posts</h1>
         <Button>Test Button</Button>
         <div className="form">
-        <label>Post title</label>
-        <input type="text" name="blogPostTitle"></input>
-        <label>Post content</label>
-        <input type="text" name="blogPostContent"></input>
-        <Button>Submit</Button>
+          <label>Post title</label>
+          <input type="text" name="blogPostTitle"></input>
+          <label>Post content</label>
+          <input type="text" name="blogPostContent"></input>
+          <Button>Submit</Button>
         </div>
-        </div>
-        );
-      }
-    }
-    
+      </div>
+    );
+  }
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const routes = require("./controller");
+const cors = require("cors");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -31,8 +32,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
+
 app.use(routes);
 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with the correct origin of your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () =>
+    console.log("Now listening at http://localhost:" + PORT + "/")
+  );
 });
