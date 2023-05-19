f5596bb2ce1f57548da5fbf0c136d4b36901e32d
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2023-05-19 15:00:18 -0500

    get all posts route made

--- server/controller/api/postRoutes.js
+++ server/controller/api/postRoutes.js
@@ -14,6 +14,15 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "We aint get posts :/" });}}
+);
+
 router.delete("/:id", async (req, res) => {
   try {
     const postData = await Post.destroy({
