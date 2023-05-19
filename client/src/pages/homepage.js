e686f3ab699369173895cd4e37af60fdf5865dfb
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2023-05-19 15:18:07 -0500

    posts render to page

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Auth from "../utils/auth";
 import Logout from "../components/logout";
+import axios from "axios";
+
 
 export default function Homepage() {
+const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get("/api/posts");
+      setPosts(response.data);
+    } catch (error){
+      console.error(error);
+    }
+  };
+  fetchPosts();
+}, []);
+
   if (!Auth.loggedIn()) {
     console.log(Auth.loggedIn);
     return window.location.assign("/init");
   } else {
+
     return (
       <div className="m-2">
          <Logout />
@@ -20,6 +37,12 @@ export default function Homepage() {
 
         <div className="d-flex justify-content-center">
           <h3>Posts</h3>
+          {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.message}</h3>
+          <p>Date: {post.date_created}</p>
+        </div>
+      ))}
         </div>
       </div>
     );
