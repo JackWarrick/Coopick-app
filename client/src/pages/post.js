59c5e1b1bc23c2ff4650470d02c9fad55ce158a1
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2023-05-23 14:34:19 -0500

    front end can post

--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Logout from "../components/logout";
-//import profilepic from '../../assets/me.jpeg';
-//import "../../styles/about.css"
+import axios from "axios";
 import Auth from "../utils/auth";
 
 export default function Post() {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/api/posts", {
+        message,
+      });
+
+      // Handle successful post creation
+      console.log("New post created:", response.data);
+
+      // Redirect to home or display success message
+      // You can customize this based on your application flow
+      window.location.assign("/home");
+    } catch (error) {
+      // Handle error
+      console.error("Error creating post:", error);
+    }
+  };
+
   if (!Auth.loggedIn()) {
     console.log("Not logged in");
     return window.location.assign("/");
@@ -12,7 +33,7 @@ export default function Post() {
     console.log("Logged in");
     return (
       <div className="App">
-        <div className= "m-2">
+        <div className="m-2">
           <Logout />
           <a href="./home" className="m-2">
             <button className="btn btn-primary">Home</button>
@@ -20,11 +41,18 @@ export default function Post() {
         </div>
         <h1>Blog Posts</h1>
         <div className="form">
-          <label>Post Title</label>
-          <input type="text" name="blogPostTitle"></input>
-          <label>Post Content</label>
-          <input type="text" name="blogPostContent"></input>
-          <button className="btn btn-success">Submit</button>
+          <form onSubmit={handleSubmit}>
+            <label>Post Content</label>
+            <input
+              type="text"
+              name="blogPostContent"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className="btn btn-success" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     );
